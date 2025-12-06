2d2262c9954f2886af0a63ce8c736bfb5cdac025
Kyliee-stack <sitiatikahsari1209@gmail.com>
2025-12-06 20:52:14 +0700

    Add Echo configuration for broadcasting and private channel support

--- public/js/bootstrap.js
+++ public/js/bootstrap.js
@@ -1,4 +1,13 @@
 import axios from 'axios';
 window.axios = axios;
 
+// resources/js/bootstrap.js
+window.Echo = new Echo({
+    // ... konfigurasi lainnya ...
+});
+
+// Catatan: Jika Anda menggunakan Private Channel, pastikan Anda 
+// mengimpor dan menggunakan 'pusher-js' yang sudah diinisialisasi 
+// dengan token otentikasi (Axios/HTTP Client)
+
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
